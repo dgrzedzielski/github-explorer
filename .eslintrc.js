0ab54c4b9f79a6c9e1d5db1c4a35288cb509a705
Dominik Grzedzielski <gie.dominik@gmail.com>
2021-06-18 14:29:47 +0200

    Add import plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,49 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/absolute-first': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: 'react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@react*',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '*.scss',
+            group: 'index',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+  },
 };
