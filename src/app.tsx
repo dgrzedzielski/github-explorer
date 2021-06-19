1ee6f9c70afba12f370213f897bf45fdd0089317
Dominik Grzedzielski <gie.dominik@gmail.com>
2021-06-19 19:40:52 +0200

    Refactor layout, add padding for main section

--- src/app.tsx
+++ src/app.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { BaseLoader } from 'components/base-loader';
-import { FormSearchUser } from 'components/form-search-user/form-search-user';
-import { PageHeader } from 'components/page-header';
+import { DefaultLayout } from 'components/default-layout';
 import { routes } from 'routes';
 
 const Homepage = React.lazy(() => import('pages/search-results'));
 const UserProfilePage = React.lazy(() => import('pages/user-profile'));
 
-function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <PageHeader>
-        <FormSearchUser />
-      </PageHeader>
-      <main>{children}</main>
-    </div>
-  );
-}
-
 function App() {
   return (
     <Router>
-      <Layout>
+      <DefaultLayout>
         <React.Suspense fallback={<BaseLoader />}>
           <Switch>
             <Route exact path={routes.search} component={Homepage} />
             <Route path={routes.profile} component={UserProfilePage} />
           </Switch>
         </React.Suspense>
-      </Layout>
+      </DefaultLayout>
     </Router>
   );
 }
---
+++ src/components/default-layout/default-layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FormSearchUser } from 'components/form-search-user';
+import './default-layout.scss';
+
+type DefaultLayoutProps = {
+  children: React.ReactNode;
+};
+
+export function DefaultLayout({ children }: DefaultLayoutProps) {
+  return (
+    <div className="layout">
+      <header className="layout__header">
+        <FormSearchUser />
+      </header>
+      <main className="layout__main">{children}</main>
+    </div>
+  );
+}
