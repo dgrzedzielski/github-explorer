9c8c03a1f81511c5262e9b4b911739355b1f352c
Dominik Grzedzielski <gie.dominik@gmail.com>
2021-06-21 11:00:17 +0200

    Add loader styles

--- src/components/base-loader/base-loader.tsx
+++ src/components/base-loader/base-loader.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import './base-loader.scss';
 
 export function BaseLoader() {
-  return <div aria-label="Loading">Loading...</div>;
+  return (
+    <div className="base-loader__container">
+      <div className="base-loader" aria-label="Loading">
+        <div className="base-loader__dot" aria-hidden="true" />
+        <div className="base-loader__dot" aria-hidden="true" />
+        <div className="base-loader__dot" aria-hidden="true" />
+      </div>
+    </div>
+  );
 }
